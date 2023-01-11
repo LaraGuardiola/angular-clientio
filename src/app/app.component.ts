5dfb5a1e0a571f508f3d5949a440eab59c52cdee
Sergio <webdev.slara@gmail.com>
2023-01-11 20:36:08 +0100

    basic client socket io

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ConnectService } from './services/connect.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+  @ViewChild('input') input: ElementRef | any
+  @ViewChild('chat') chat: ElementRef | any
+  constructor(protected connectService: ConnectService){
+    connectService.socket.on("response", (input:string) => {
+      let chatBox = document.createElement("p");
+      chatBox.innerText = input;
+      this.chat.nativeElement.append(chatBox);
+    })
+  }
+
+  
+ 
+}
+
+
+ 
+
+
+
---
+++ src/app/services/connect.service.ts
@@ -0,0 +1,33 @@
+import { ElementRef, Injectable, ViewChild } from '@angular/core';
+import { io } from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConnectService {
+  socket = io('http://localhost:3000');
+  
+  constructor() {
+    this.socket.on("connect", () => {
+      console.log("Connected - SocketID:",this.socket.id)
+    })
+   }
+
+   write(chatElem: HTMLDivElement, inputElem: HTMLInputElement, input: string){
+    console.log(input)
+    this.socket.emit("message", input)
+    inputElem.value =""
+    this.appendText(chatElem, input)
+   }
+
+   onWriting(length: number){
+    if(length === 0) return
+    this.socket.emit("inputMessage", length)
+   }
+
+   appendText(chat: HTMLDivElement, input: string){
+    let chatBox = document.createElement("p");
+    chatBox.innerText = input;
+    chat.append(chatBox);
+   }
+}
