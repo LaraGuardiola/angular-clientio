956625c7dfa477d368d8badbd6c654b391c729a8
Sergio <webdev.slara@gmail.com>
2023-01-13 15:39:58 +0100

    changed url

--- src/app/services/connect.service.ts
+++ src/app/services/connect.service.ts
@@ -6,7 +6,7 @@ import { io } from 'socket.io-client';
 })
 export class ConnectService {
 
-  socket = io('http://localhost:3000');
+  socket = io('https://nodejs-socketio-production.up.railway.app/');
   
   constructor() {
     this.socket.on("connect", () => {
