bd780f9a661080fdfb75fabfa6d1685f110020a5
Sergio <webdev.slara@gmail.com>
2023-01-15 20:45:08 +0100

    added response afterViewInit so I can access DOM

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ConnectService } from 'src/app/services/connect.service';
 
 @Component({
@@ -6,13 +6,18 @@ import { ConnectService } from 'src/app/services/connect.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewInit{
   @ViewChild('chat') chat: ElementRef | any
   placeholderValue: string = "Write your message here..."
 
   constructor(protected connectService: ConnectService){
-    connectService.socket.on("response", (input:string) => {
-      connectService.appendText(this.chat,input)
+    // connectService.socket.on("response", (input:string) => {
+    //   connectService.appendText(this.cha as HTMLDivElement, input)
+    // })
+  }
+  ngAfterViewInit(): void {
+    this.connectService.socket.on("response", (input:string) => {
+      this.connectService.appendText(this.chat, input)
     })
   }
 }
